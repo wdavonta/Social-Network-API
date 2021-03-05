af9838089d727efc1627b634132d86685c36b863
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-03-05 17:28:36 -0500

    update user route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -8,3 +8,29 @@ const {
   addFriend,
   deleteFriend
 } = require('../../controllers/user-controller');
+
+
+// Set up GET all and POST at /api/users
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+
+// Set up GET one, PUT, and DELETE at /api/user/:id
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+
+
+// Set up POST, DELETE at /api/users/:userId/friends/:friendId
+router
+.route('/:userId/friends/:friendsId')
+.post(addFriend)
+.delete(deleteFriend)
+
+
+module.exports = router;
