5fbb332d7b5f987a6e5ac37782a6b8d84e5d5892
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-03-06 17:51:17 -0500

    added delete and add friends

--- contollers/thought-controller.js
+++ contollers/thought-controller.js
@@ -9,7 +9,6 @@ const thoughtController = {
         select: '-__v'
       })
       .select('-__v')
-      .sort({ _id: -1 })
       .then(dbThoughtData => res.json(dbThoughtData))
       .catch(err => {
         console.log(err);
--- contollers/user-controller.js
+++ contollers/user-controller.js
@@ -38,7 +38,7 @@ const userController = {
       .catch(err => res.json(err));
   },
 
-// update pizza by id
+// update user by id
 updateUser({ params, body }, res) {
     User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then(dbUserData => {
@@ -51,12 +51,56 @@ updateUser({ params, body }, res) {
       .catch(err => res.status(400).json(err));
   },
 
-  // delete pizza
+  // delete user
   deleteUser({ params }, res) {
     User.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+    if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+      return Thought.deleteMany({ userId: params.od})
+    })
       .then(dbUserData => res.json(dbUserData))
-      .catch(err => res.json(err));
-  }
+      .catch(err => res.status(400).json(err));
+  },
+
+
+
+    // add friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: { friendId: friendId } } },
+            { new: true, runValidators: true}
+        )
+
+        .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No user found with this id!' });
+              return;
+            };
+            return res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+
+
+    // delete friend
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId }},
+            { new: true}
+          )
+
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+          
+     }
+
+
 };
 
 module.exports = userController;
